5609ef848ef1edb133907779dd648a568c2de6ed
btrzslv <143760674+btrzslv@users.noreply.github.com>
2024-02-28 23:27:52 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+<>
+{<h1>Preciso do meu notebook urgente!</h1>}
+</>
+)
+}
+
+export default App
